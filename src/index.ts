4518873b18a003ea2eb781a57a91f2842a486524
mo36924 <mo36924@users.noreply.github.com>
2021-01-09 01:00:14 +0900

    ✨

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import type { default as _babel, PluginObj } from "@babel/core";
 import babelPluginMinifyDeadCodeElimination from "babel-plugin-minify-dead-code-elimination";
 
 export default (babel: typeof _babel, options: any): PluginObj => {
-  const { visitor } = babelPluginMinifyDeadCodeElimination(babel, options);
+  const visitor = babelPluginMinifyDeadCodeElimination(babel, options).visitor;
+
+  for (const visitNode of Object.values<any>(visitor)) {
+    if (visitNode && !visitNode.enter && visitNode.exit) {
+      visitNode.enter = visitNode.exit;
+    }
+  }
 
   return {
     name: "dead-code-elimination",
-    visitor: {
-      ...visitor,
-      Program: {
-        enter: typeof visitor.Program === "object" ? visitor.Program.exit : visitor.Program,
-        ...visitor.Program,
-      },
-    },
+    visitor: visitor,
   };
 };
